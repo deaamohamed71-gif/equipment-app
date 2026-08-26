6a6c6b21e4564fe5172607d4c295d9771cb31766
deaamohamed71-gif <deaamohamed71@gmail.com>
2026-08-26 21:29:06 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
-self.addEventListener('fetch', function (event) {
-  // ملف افتراضي لتشغيل الـ PWA
+const CACHE_NAME = 'equipment-app-v1';
+const assetsToCache = [
+  '/equipment-app/',
+  '/equipment-app/index.html',
+  '/equipment-app/manifest.json',
+  '/equipment-app/icon-192.png',
+  '/equipment-app/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
